a3fc86bc2ef7459328a07798e82f720666b048a0
Wyne10 <izmodenov1997@gmail.com>
2024-12-29 13:07:07 +0500

    Make mod client only

--- src/main/java/me/wyne/RePacker.java
+++ src/main/java/me/wyne/RePacker.java
@@ -1,12 +1,11 @@
 package me.wyne;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class RePacker implements ModInitializer, ClientModInitializer {
+public class RePacker implements ClientModInitializer {
 	public static final String MOD_ID = "repacker";
 
 	// This logger is used to write text to the console and the log file.
@@ -15,17 +14,8 @@ public class RePacker implements ModInitializer, ClientModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
-
-	@Override
 	public void onInitializeClient() {
-
+		LOGGER.info("Hello Fabric world!");
 	}
 
 }
